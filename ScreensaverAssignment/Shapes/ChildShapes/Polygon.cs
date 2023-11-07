8ba7ac16ab8ea4195ae886dccd9c8320b2d54536
W0471965 <joyceding007@gmail.com>
2023-11-07 11:08:06 -0400

    Pentagon draw

--- ScreensaverAssignment/Shapes/ChildShapes/Polygon.cs
+++ ScreensaverAssignment/Shapes/ChildShapes/Polygon.cs
@@ -12,11 +12,9 @@ namespace ScreensaverAssignment.Shapes.ChildShapes
     public class Polygon : Shape
     {
 
-        private Point[] points; // Array to store the vertices of the polygon
         private Random random = new Random();
-        private int[] randomPoints = new int[6];
-        private int width = 50; 
-        private int height = 50;
+        private int radius = 50; 
+        private int sides = 5;
         private Color color;
         public int topX;
         public int topY;
@@ -26,58 +24,57 @@ namespace ScreensaverAssignment.Shapes.ChildShapes
         {
             this.topX = x;
             this.topY = y;
-
-            for (int i = 0; i < 6; i++)
-            {
-                randomPoints[i] = random.Next(1, 100); // Generates random integers from 1 to 100
-            }
-            // Initialize the vertices of the polygon
-            points = new Point[4];
-            points[0] = new Point(topX, topY);
-            points[1] = new Point(topX + width, topY);
-            points[2] = new Point(topX + width, topY+height);
-            points[3] = new Point(topX, topY + height);
-            color = Color.FromArgb(255, random.Next(255), random.Next(255), random.Next(255));
-        }
+         }
 
         public override void Draw(PaintEventArgs e, Form form)
         {
+            Point[] points = new Point[sides];
+
+            for (int i = 0; i < sides; i++)
+            {
+                double angle = 2 * Math.PI * i / sides;
+                int x1 = topX + (int)(radius * Math.Cos(angle));
+                int y1 = topY + (int)(radius * Math.Sin(angle));
+                points[i] = new Point(x1, y1);
+            }
+
+            color = Color.FromArgb(255, random.Next(255), random.Next(255), random.Next(255));
             SolidBrush brush = new SolidBrush(color);
             e.Graphics.FillPolygon(brush, points);
         }
 
         public override void Move(Form form)
         {
-            XVelocity = random.Next(-30, 30); // Adjust the range as needed
-            YVelocity = random.Next(-30, 30);
+            //XVelocity = random.Next(-30, 30); // Adjust the range as needed
+            //YVelocity = random.Next(-30, 30);
 
-            this.topX += XVelocity;
-            this.topY += YVelocity;
+            //this.topX += XVelocity;
+            //this.topY += YVelocity;
 
-            points[0] = new Point(topX, topY);
-            points[1] = new Point(topX + width, topY);
-            points[2] = new Point(topX + width, topY + height);
-            points[3] = new Point(topX, topY + height);
+            //points[0] = new Point(topX, topY);
+            //points[1] = new Point(topX + width, topY);
+            //points[2] = new Point(topX + width, topY + height);
+            //points[3] = new Point(topX, topY + height);
 
-            topX += XVelocity;
-            topY += YVelocity;
-            leftX += XVelocity;
-            leftY += YVelocity;
-            rightX += XVelocity;
-            rightY += YVelocity;
+            //topX += XVelocity;
+            //topY += YVelocity;
+            //leftX += XVelocity;
+            //leftY += YVelocity;
+            //rightX += XVelocity;
+            //rightY += YVelocity;
 
-            if (topX < 0 || (topX + width) > form.ClientRectangle.Right)
-            {
-                XVelocity = -XVelocity; // Reverse horizontal velocity on collision with left or right wall
-            }
+            //if (topX < 0 || (topX + width) > form.ClientRectangle.Right)
+            //{
+            //    XVelocity = -XVelocity; // Reverse horizontal velocity on collision with left or right wall
+            //}
 
-            if (topY < 0 || (topY + height) > form.ClientRectangle.Bottom)
-            {
-                YVelocity = -YVelocity; // Reverse vertical velocity on collision with top or bottom wall
-            }
+            //if (topY < 0 || (topY + height) > form.ClientRectangle.Bottom)
+            //{
+            //    YVelocity = -YVelocity; // Reverse vertical velocity on collision with top or bottom wall
+            //}
 
             
-            form.Invalidate();
+            //form.Invalidate();
         }
 
     }
